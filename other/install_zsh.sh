f8afd40bd42249d241165fe237e2d5ea44937890
rickycraft <ricky.roveri@gmail.com>
2020-09-05 21:59:27 +0200

    [ZSH] finalized install script

--- other/install_zsh.sh
+++ other/install_zsh.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+
 cp .zshrc $HOME
 cp .p10k.zsh $HOME
 cp .tmux.conf $HOME
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
+zsh
