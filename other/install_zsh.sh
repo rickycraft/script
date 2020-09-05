e94e88657790fe9d71e8789642a97f83e8c018f4
rickycraft <ricky.roveri@gmail.com>
2020-09-05 23:44:46 +0200

    [ZSH] install_zsh removes git icon

--- other/install_zsh.sh
+++ other/install_zsh.sh
@@ -10,4 +10,12 @@ cp .tmux.conf $HOME
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
+cd $HOME/.oh-my-zsh/custom/themes/powerlevel10k/internal
+GIT_ICON_LINE=$(grep 'function _p9k_vcs_icon() {' p10k.zsh -n | awk -F ':' '{print $1}')
+START_LINE=$(( $GIT_ICON_LINE + 1))
+END_LINE=$(( $START_LINE + 6))
+mv p10k.zsh p10k.zsh.tmp
+sed $START_LINE','$END_LINE'd' p10k.zsh.tmp > p10k.zsh
+cd $HOME
+
 zsh
