a09becf57e3d0e467df8c50a704f9b9fac7a5e5f
rickycraft <ricky.roveri@gmail.com>
2020-09-08 22:46:24 +0200

    [BREAKING] renaming and moving

--- bash/install_zsh.sh
+++ bash/install_zsh.sh
@@ -3,6 +3,7 @@
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 
+cd ../dotfiles
 cp .zshrc $HOME
 cp .p10k.zsh $HOME
 cp .tmux.conf $HOME
