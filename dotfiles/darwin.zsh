261c2e519d3390c3b80e685e32db3fcfb7e7095e
rickycraft <ricky.roveri@gmail.com>
2020-09-10 23:35:40 +0200

    mac config moved to separate file, fix install

---
+++ dotfiles/darwin.zsh
@@ -0,0 +1,13 @@
+# Mac zsh config
+
+alias cask='brew cask'
+alias bupdate='brew update && brew upgrade; && cask upgrade $(cask list) &&  bubc'
+
+export GIT_HOME_DIR=$HOME/Git
+export SCRIPT=$HOME/Git/script
+export ADB_SYNC_DEST="/Volumes/CROCCANTE/OnePlus"
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+export SDKROOT=macosx10.14
+
+source $ZSH_PLUGINS/osx/osx.plugin.zsh
+source $ZSH_PLUGINS/brew/brew.plugin.zsh
