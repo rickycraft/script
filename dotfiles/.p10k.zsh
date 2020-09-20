0d868a4c446f37e86703c9cbd75fe5b364c30a78
rickycraft <ricky.roveri@gmail.com>
2020-09-20 22:20:10 +0200

    Added python env to p10k

--- dotfiles/.p10k.zsh
+++ dotfiles/.p10k.zsh
@@ -52,7 +52,7 @@
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
-    # virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
+    virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
     # anaconda                # conda environment (https://conda.io/)
     # pyenv                   # python environment (https://github.com/pyenv/pyenv)
     # goenv                   # go environment (https://github.com/syndbg/goenv)
