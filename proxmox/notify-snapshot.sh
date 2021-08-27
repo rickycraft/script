bd4ab88b62fd8c30415dda8a945442f1bcf1db3e
Riccardo <ricky.roveri@gmail.com>
2021-08-27 09:56:47 +0200

    [PROXMOX] added notify script

---
+++ proxmox/notify-snapshot.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+NODO=""
+CLIENTE=""
+DATA=$(date +%F)
+zfs list -t snap | mail -s "PVE_SNAP $CLIENTE $NODO $DATA" -r "pve@mailbo.it" monitoraggio@mailbo.it
---
+++ proxmox/notify-zpool.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+NODO=""
+CLIENTE=""
+DATA=$(date +%F)
+{
+  zpool list &
+  zfs list
+} | mail -s "PVE_SPAZIO $CLIENTE $NODO $DATA" -r "pve@mailbo.it" monitoraggio@mailbo.it
+zpool status | mail -s "PVE_DISCO $CLIENTE $NODO $DATA" -r "pve@mailbo.it" monitoraggio@mailbo.it
